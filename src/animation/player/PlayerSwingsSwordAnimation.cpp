502bf6a03fdccc5541a5075f009735ed08a757d3
Kebigon <git@kebigon.xyz>
2017-10-25 07:10:15 +0900

    Set test animation on PlayerSwingsSword

--- src/animation/player/PlayerSwingsSwordAnimation.cpp
+++ src/animation/player/PlayerSwingsSwordAnimation.cpp
@@ -1,6 +1,7 @@
 #include "PlayerSwingsSwordAnimation.hpp"
 
 #include "../../globals.h"
+#include "../../data/bitmaps.h"
 
 PlayerSwingsSwordAnimation::PlayerSwingsSwordAnimation(uint8_t endCounter)
 	: Animation(endCounter)
@@ -13,10 +14,19 @@ void PlayerSwingsSwordAnimation::onAnimationEnd()
 
 void PlayerSwingsSwordAnimation::display(Player *player, uint8_t displayPlayerX, uint8_t displayPlayerY)
 {
+	uint8_t frame;
+
 	switch (player->direction)
 	{
 		case Direction::LEFT:
-
+			frame = getAnimationCountdown() == 1 ? 3 : 2;
 			break;
+		case Direction::RIGHT:
+			frame = getAnimationCountdown() == 1 ? 1 : 0;
+			break;
+		default:
+			return;
 	}
+
+	Sprites::drawPlusMask(displayPlayerX, displayPlayerY, linkSword_plus_mask,  frame);
 }
