417a3e632784e15c07fb0b6ec8d50bd462de96dc
Kebigon <git@kebigon.xyz>
2017-11-09 20:38:52 +0900

    Fix DroppedItemPhysicsComponent

--- src/component/physics/DroppedItemPhysicsComponent.cpp
+++ src/component/physics/DroppedItemPhysicsComponent.cpp
@@ -4,15 +4,16 @@
 
 #include "entity/DroppedItem.hpp"
 #include "globals.h"
+#include "inventory/Inventory.hpp"
 
 void DroppedItemPhysicsComponent::update(DroppedItem *item)
 {
 	Rect itemRect = { item->getLocation()->getX(), item->getLocation()->getY(), items_plus_mask[0], items_plus_mask[1] };
-	Rect playerRect = { item->getLocation()->getX(), item->getLocation()->getY(), link_plus_mask[0], link_plus_mask[1] };
+	Rect playerRect = { game->getPlayer()->getLocation()->getX(), game->getPlayer()->getLocation()->getY(), link_plus_mask[0], link_plus_mask[1] };
 
 	if (arduboy.collide(itemRect, playerRect))
 	{
-		// game->getPlayer()->getInventory()->addItem(item->getItemStack());
+		game->getPlayer()->getInventory()->addItem(item->getItemStack());
 		game->removeEntity(item);
 	}
 }
