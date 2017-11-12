36de3275e296a0b0ff2ec3096b5037a5be6f7177
Kebigon <git@kebigon.xyz>
2017-11-12 21:46:33 +0900

    Update LocationTest

--- test/LocationTest.cpp
+++ test/LocationTest.cpp
@@ -11,17 +11,17 @@ Describe(Location)
 BeforeEach(Location) {}
 AfterEach(Location) {}
 
-Ensure(Location, test_max_coordinates)
+Ensure(Location, test_works_with_normal_coordinates)
 {
-	Location *locMax = new Location(nullptr, MAX_COORDINATE, MAX_COORDINATE);
-	assert_that(locMax->getX(), is_equal_to(MAX_COORDINATE));
-	assert_that(locMax->getY(), is_equal_to(MAX_COORDINATE));
-	assert_that(locMax->getTileX(), is_equal_to(MAX_COORDINATE / 16));
-	assert_that(locMax->getTileY(), is_equal_to(MAX_COORDINATE / 16));
-	free(locMax);
+	Location *loc = new Location(nullptr, 1234, 567);
+	assert_that(loc->getX(), is_equal_to(1234));
+	assert_that(loc->getY(), is_equal_to(567));
+	assert_that(loc->getTileX(), is_equal_to(1234 / 16));
+	assert_that(loc->getTileY(), is_equal_to(567 / 16));
+	free(loc);
 }
 
-Ensure(Location, test_min_coordinates)
+Ensure(Location,  works_with_min_coordinates)
 {
 	Location *locMin = new Location(nullptr, MIN_COORDINATE, MIN_COORDINATE);
 	assert_that(locMin->getX(), is_equal_to(MIN_COORDINATE));
@@ -31,10 +31,21 @@ Ensure(Location, test_min_coordinates)
 	free(locMin);
 }
 
+Ensure(Location, works_with_max_coordinates)
+{
+	Location *locMax = new Location(nullptr, MAX_COORDINATE, MAX_COORDINATE);
+	assert_that(locMax->getX(), is_equal_to(MAX_COORDINATE));
+	assert_that(locMax->getY(), is_equal_to(MAX_COORDINATE));
+	assert_that(locMax->getTileX(), is_equal_to(MAX_COORDINATE / 16));
+	assert_that(locMax->getTileY(), is_equal_to(MAX_COORDINATE / 16));
+	free(locMax);
+}
+
 TestSuite *location_tests()
 {
 	TestSuite *suite = create_test_suite();
-	add_test_with_context(suite, Location, test_max_coordinates);
-	add_test_with_context(suite, Location, test_min_coordinates);
+	add_test_with_context(suite, Location, test_works_with_normal_coordinates);
+	add_test_with_context(suite, Location, works_with_min_coordinates);
+	add_test_with_context(suite, Location, works_with_max_coordinates);
 	return suite;
 }
