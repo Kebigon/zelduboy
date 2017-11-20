0d8183e01b931add49644d0233cacc15833193a8
Kebigon <git@kebigon.xyz>
2017-11-20 23:28:29 +0900

    Add collision box to entities

--- src/entity/Entity.cpp
+++ src/entity/Entity.cpp
@@ -1,5 +1,7 @@
 #include "Entity.hpp"
 
+#include "globals.h"
+
 Entity::Entity(Location * location)
 	: location(location)
 {}
@@ -7,6 +9,18 @@ Entity::Entity(Location * location)
 Location * Entity::getLocation() const
 {
 	return location;
+}
+
+// TODO: At the moment all entites are 16*16 for test, to change in the future
+Rect Entity::getCollisionBox() const
+{
+	Rect collisionBox = { location->getX(), location->getY(), 16, 16 };
+	return collisionBox;
+}
+
+bool Entity::collideWith(Rect otherCollisionBox) const
+{
+	return arduboy.collide(getCollisionBox(), otherCollisionBox);
 }
 
 bool Entity::isPassable() const
--- src/entity/Entity.hpp
+++ src/entity/Entity.hpp
@@ -1,7 +1,8 @@
 #ifndef ENTITY_HPP
 #define ENTITY_HPP
 
-#include "../Location.hpp"
+#include "Location.hpp"
+#include <Arduboy2.h>
 
 class Entity
 {
@@ -10,6 +11,8 @@ class Entity
 public:
 	Entity(Location *location);
 	Location * getLocation() const;
+	Rect getCollisionBox() const;
+	bool collideWith(Rect otherCollisionBox) const;
 	virtual bool isPassable() const;
 	virtual void update() = 0;
 	virtual void draw() = 0;
